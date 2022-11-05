844a2cbf03a54b3b11cf4f1c17b535f245b71ca1
fares <fhalasahhh@gmail.com>
2022-11-05 15:24:25 +0300

    using live domain url done

--- src/configs.ts
+++ src/configs.ts
@@ -1,5 +1,5 @@
-export const domain = 'clikstaging.herokuapp.com';
-export const STAGING_URI = `https://${domain}/graphql`;
-export const UPLOAD_URI = `https://${domain}/uploads/public`;
-export const VIEW_UPLOAD_URI = `https://${domain}/uploads/`;
-export const REST_URI = `https://${domain}`;
+export const domain = 'http://clikjo.com/';
+export const STAGING_URI = `${domain}/api/graphql`;
+export const UPLOAD_URI = `${domain}/api/uploads/public`;
+export const VIEW_UPLOAD_URI = `${domain}/api/uploads/`;
+export const REST_URI = `${domain}/api`;
